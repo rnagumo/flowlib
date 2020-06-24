553f0c2e1783a9236dd33d567f2e6ebac0b28368
Ryosuke Nagumo <ryosuke.nagumo125@gmail.com>
2020-06-24 21:11:46 +0900

    Fix test case for glow

--- tests/test_glow.py
+++ tests/test_glow.py
@@ -16,34 +16,38 @@ class TestGlow(unittest.TestCase):
         self.assertTupleEqual(self.model._prior_var.size(), (3, 32, 32))
 
     def test_forward(self):
-        x = torch.randn(2, 3, 32, 32)
+        x = torch.rand(2, 3, 32, 32)
         z = self.model(x)
 
         self.assertTupleEqual(z.size(), x.size())
+        self.assertFalse(torch.isnan(z).any())
 
     def test_loss_func(self):
-        x = torch.randn(2, 3, 32, 32)
+        x = torch.rand(2, 3, 32, 32)
         loss_dict = self.model.loss_func(x)
 
         self.assertIsInstance(loss_dict, dict)
         self.assertGreater(loss_dict["loss"], 0)
 
     def test_inference(self):
-        x = torch.randn(2, 3, 32, 32)
+        x = torch.rand(2, 3, 32, 32)
         z, logdet = self.model.inference(x)
 
         self.assertTupleEqual(z.size(), x.size())
+        self.assertFalse(torch.isnan(z).any())
         self.assertTupleEqual(logdet.size(), ())
 
     def test_inverse(self):
-        z = torch.randn(2, 3, 32, 32)
+        z = torch.rand(2, 3, 32, 32)
         x = self.model.inverse(z)
 
         self.assertTupleEqual(x.size(), z.size())
+        self.assertFalse(torch.isnan(x).any())
 
     def test_sample(self):
         x = self.model.sample(5)
         self.assertTupleEqual(x.size(), (5, 3, 32, 32))
+        self.assertFalse(torch.isnan(x).any())
 
 
 if __name__ == "__main__":
