949bb284f3bdc90e438ad691d41fd26b53c526e2
Ryosuke Nagumo <ryosuke.nagumo125@gmail.com>
2020-06-25 20:26:40 +0900

    Update glow model

--- flowlib/glow.py
+++ flowlib/glow.py
@@ -16,9 +16,9 @@ from torch import Tensor, nn
 from .base import FlowModel
 from .conv import InvertibleConv
 from .coupling import AffineCoupling
-from .neuralnet import ResNet
+from .neuralnet import Conv2dZeros
 from .normalization import ActNorm2d
-from .operation import Squeeze, Unsqueeze, Preprocess
+from .operation import Squeeze, ChannelwiseSplit, Preprocess
 
 
 class ScaleTranslateNet(nn.Module):
@@ -32,8 +32,13 @@ class ScaleTranslateNet(nn.Module):
     def __init__(self, in_channels: int, mid_channels: int):
         super().__init__()
 
-        self.resnet = ResNet(
-            in_channels, mid_channels, in_channels * 2, num_blocks=8)
+        self.conv = nn.Sequential(
+            nn.Conv2d(in_channels, mid_channels, 3, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(mid_channels, mid_channels, 1),
+            nn.ReLU(),
+            Conv2dZeros(mid_channels, in_channels * 2, 3, padding=1),
+        )
 
     def forward(self, x: Tensor) -> Tuple[Tensor, Tensor]:
         """Forward propagation.
@@ -46,7 +51,7 @@ class ScaleTranslateNet(nn.Module):
             t (torch.Tensor): t value, size `(b, c, h, w)`.
         """
 
-        s_t = self.resnet(x)
+        s_t = self.conv(x)
         log_s, t = torch.chunk(s_t, 2, dim=1)
         log_s = torch.tanh(log_s)
 
@@ -60,24 +65,41 @@ class Glow(FlowModel):
         in_channels (int, optional): Number of channels in input image.
         mid_channels (int, optional): Number of channels in mid image.
         image_size (int, optional): Size of input image.
+        depth (int, optional): Depth of flow `K`.
+        level (int, optional): Number of levels `L`.
     """
 
-    def __init__(self, in_channels: int = 3, mid_channels: int = 64,
-                 image_size: int = 32):
-        super().__init__(in_size=(in_channels, image_size, image_size))
+    def __init__(self, in_channels: int = 3, mid_channels: int = 512,
+                 image_size: int = 32, depth: int = 32, level: int = 3):
+        super().__init__((in_channels * 2 ** (level + 1),
+                          image_size // 2 ** level, image_size // 2 ** level))
 
-        flow_list = [Preprocess(), Squeeze()]
+        flow_list = [Preprocess()]
+
+        current_channels = in_channels
 
         # Main blocks
-        for _ in range(3):
-            flow_list += [
-                ActNorm2d(in_channels * 4),
-                InvertibleConv(in_channels * 4),
-                AffineCoupling(
-                    ScaleTranslateNet(in_channels * 4, mid_channels * 2),
-                    mask_type="channel_wise", inverse_mask=False),
-            ]
+        for i in range(level):
+            current_channels *= 4
 
-        flow_list += [Unsqueeze()]
+            # 1. Squeeze
+            flow_list += [Squeeze()]
+
+            # 2. K steps of flow
+            for _ in range(depth):
+                flow_list += [
+                    ActNorm2d(current_channels),
+                    InvertibleConv(current_channels),
+                    AffineCoupling(
+                        ScaleTranslateNet(current_channels, mid_channels),
+                        mask_type="channel_wise", inverse_mask=False),
+                ]
+
+            # 3. Split
+            if i < level - 1:
+                flow_list += [ChannelwiseSplit(current_channels)]
+
+            # Channel size is halved
+            current_channels //= 2
 
         self.flow_list = nn.ModuleList(flow_list)
