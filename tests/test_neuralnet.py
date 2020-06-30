c8afe1df80d4fed8c64826825ce2d52c6306af71
Ryosuke Nagumo <ryosuke.nagumo125@gmail.com>
2020-06-30 10:40:08 +0900

    Add test case of conv 2d zeros

--- tests/test_neuralnet.py
+++ tests/test_neuralnet.py
@@ -6,6 +6,19 @@ import torch
 import flowlib
 
 
+class TestConv2dZeros(unittest.TestCase):
+
+    def setUp(self):
+        self.model = flowlib.Conv2dZeros(3, 3, 3, padding=1)
+
+    def test_forward(self):
+        x = torch.randn(4, 3, 8, 8)
+        z = self.model(x)
+
+        self.assertTupleEqual(z.size(), x.size())
+        self.assertFalse(torch.isnan(z).any())
+
+
 class TestResidualBlock(unittest.TestCase):
 
     def setUp(self):
