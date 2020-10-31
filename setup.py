d02bf5be9924732b5d2d8dbd9717c2e8c18c3711
Ryosuke Nagumo <ryosuke.nagumo125@gmail.com>
2020-10-31 14:19:17 +0900

    Update requirements

--- setup.py
+++ setup.py
@@ -3,30 +3,30 @@ from setuptools import setup, find_packages
 
 install_requires = [
     "torch>=1.6",
+    "torchvision>=0.7",
 ]
 
 
 extras_require = {
-    "dev": [
-        "pytest",
-        "black",
-        "flake8",
-        "mypy",
-    ],
-    "example": [
-        "torchvision>=0.7",
+    "training": [
         "numpy>=1.19",
         "pandas>=1.0",
         "matplotlib>=3.2",
         "tqdm>=4.48",
         "tensorboardX>=2.1",
     ],
+    "dev": [
+        "pytest",
+        "black",
+        "flake8",
+        "mypy==0.790",
+    ],
 }
 
 
 setup(
     name="flowlib",
-    version="0.1",
+    version="0.2",
     description="Flow models in PyTorch",
     packages=find_packages(),
     install_requires=install_requires,
